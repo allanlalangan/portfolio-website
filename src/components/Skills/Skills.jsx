60ce5cc6253df351136bde58809b43a57e270497
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-03 23:38:05 -0700

    feat(Skills): update alt text for skill icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,48 +6,48 @@ const Skills = () => {
   return (
     <article className={styles.skills}>
       <img
-        alt='icon'
+        alt='html5 icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='css3 icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='javascript icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='reactjs icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='redux icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='sass icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg'
       />
 
       <img
-        alt='icon'
+        alt='materialui icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg'
       />
 
-      <FaGithub className={styles['skills__icon']} />
+      <FaGithub aria-label='github icon' className={styles['skills__icon']} />
       {/* <img
         alt='icon'
         className={styles['svg-icon']}
@@ -55,7 +55,7 @@ const Skills = () => {
       /> */}
 
       <img
-        alt='icon'
+        alt='npm icon'
         className={styles['svg-icon']}
         src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg'
       />
