04a5575b9d5fef9c80008556d1f4de20b6adee0a
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-11 13:12:47 -0700

    feat: style bezels section, add description and tech to ProjectCard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,42 @@
 import ProjectLinks from '../ProjectLinks/ProjectLinks';
 import styles from './ProjectCard.module.scss';
 
+import desktop from '../../assets/bezels/desktop.png';
+import tablet from '../../assets/bezels/tablet.png';
+import mobile from '../../assets/bezels/mobile.png';
+
 const ProjectCard = ({ project: { title, description, tech } }) => {
   return (
-    <article className={styles['project-header']}>
+    <article className={styles['container']}>
       <h2 className={styles['project-header__title']}>{title}</h2>
       <ProjectLinks />
+      <section className={styles['bezels-container']}>
+        <figure className={styles.bezels}>
+          <img
+            src={desktop}
+            alt='project on desktop'
+            className={styles['desktop']}
+          />
+          <img
+            src={tablet}
+            alt='project on tablet'
+            className={styles['tablet']}
+          />
+          <img
+            src={mobile}
+            alt='project on mobile'
+            className={styles['mobile']}
+          />
+        </figure>
+      </section>
+      <section>
+        <p>{description}</p>
+        <ul>
+          {tech?.map((t) => (
+            <li>{t}</li>
+          ))}
+        </ul>
+      </section>
     </article>
   );
 };
--- src/components/ProjectLinks/ProjectLinks.jsx
+++ src/components/ProjectLinks/ProjectLinks.jsx
@@ -3,23 +3,27 @@ import { FaGithub, FaLink } from 'react-icons/fa';
 
 const ProjectLinks = () => {
   return (
-    <div className={styles['button-group']}>
-      <button
-        className={`${styles['project-link-button']} ${styles['project-link-button--github']}`}
-      >
-        <FaGithub className={styles['project-link-button__icon']} />
-        <span className={styles['project-link-button__text']}>Source Code</span>
-      </button>
-      <div className={styles['button-divider']} />
-      <button
-        className={`${styles['project-link-button']} ${styles['project-link-button--live']}`}
-      >
-        <FaLink className={styles['project-link-button__icon']} />
-        <span className={styles['project-link-button__text']}>
-          Live Website
-        </span>
-      </button>
-    </div>
+    <section className={styles.container}>
+      <div className={styles['button-group']}>
+        <button
+          className={`${styles['project-link-button']} ${styles['project-link-button--github']}`}
+        >
+          <FaGithub className={styles['project-link-button__icon']} />
+          <span className={styles['project-link-button__text']}>
+            Source Code
+          </span>
+        </button>
+        <div className={styles['button-divider']} />
+        <button
+          className={`${styles['project-link-button']} ${styles['project-link-button--live']}`}
+        >
+          <FaLink className={styles['project-link-button__icon']} />
+          <span className={styles['project-link-button__text']}>
+            Live Website
+          </span>
+        </button>
+      </div>
+    </section>
   );
 };
 export default ProjectLinks;
